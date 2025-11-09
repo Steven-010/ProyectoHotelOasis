b46abfc6e418db7ae7e64cf890fb7d458030d228
Steven-010 <L22230714@minatitlan.tecnm.mx>
2025-11-08 20:22:17 -0600

    db.js

--- ProyectoHotelOasis/backend/db.js
+++ ProyectoHotelOasis/backend/db.js
@@ -1,18 +1,25 @@
 const mysql = require('mysql2');
 
-// Crea el "puente" de conexión con tus credenciales
+// Crea el "puente" de conexión utilizando las variables de entorno de Railway
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // tu usuario MySQL
-    password: '16Di1983', // tu contraseña MySQL
-    database: 'hotel_oasis_db'
+    // Usa la variable HOST que configuraste en Railway
+    host: process.env.HOST, 
+    
+    // Usa la variable USER que configuraste en Railway
+    user: process.env.USER, 
+    
+    // Usa la variable PASSWORD que configuraste en Railway
+    password: process.env.PASSWORD, 
+    
+    // Usa la variable DATABASE que configuraste en Railway
+    database: process.env.DATABASE
 });
 
 
 // Verificamos si la conexión funciona
 connection.connect(error => {
     if (error) {
-        console.error('Error al conectar a la base de datos:', error);
+        console.error('Error al conectar a la base de datos:', error); 
         return;
     }
     console.log('¡Conexión exitosa a la base de datos!');
