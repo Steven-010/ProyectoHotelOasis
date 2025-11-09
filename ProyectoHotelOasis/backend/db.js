056ca13ac0a2b0fcfaebdf4601f4fd78bb1418ca
Steven-010 <L22230714@minatitlan.tecnm.mx>
2025-11-08 22:27:03 -0600

    Ajustes de despliegue y subida de archivos pendientes

--- ProyectoHotelOasis/backend/db.js
+++ ProyectoHotelOasis/backend/db.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql2');
 
-// Crea el "puente" de conexión con tus credenciales
+// Crea la conexión usando las variables correctas de Railway
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // tu usuario MySQL
-    password: '16Di1983', // tu contraseña MySQL
-    database: 'hotel_oasis_db'
+    host: process.env.DB_HOST,       // mysql.railway.internal
+    user: process.env.DB_USER,       // root
+    password: process.env.DB_PASSWORD, // tu contraseña de railway
+    database: process.env.DB_NAME,     // ferrocarril
+    port: process.env.DB_PORT        // 3306
 });
-
 
 // Verificamos si la conexión funciona
 connection.connect(error => {
     if (error) {
-        console.error('Error al conectar a la base de datos:', error);
+        console.error(' Error al conectar a la base de datos:', error);
         return;
     }
-    console.log('¡Conexión exitosa a la base de datos!');
+    console.log(' ¡Conexión exitosa a la base de datos en Railway!');
 });
 
 module.exports = connection;
